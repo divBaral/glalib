469f3102d7a761d80560f1f7cfc82a4ea17cf558
divBaral <076bct068.saroj@pcampus.edu.np>
2022-07-10 01:09:50 +0545

    Matrix Multiplication fixed

--- src/MatrixTransformation2.cpp
+++ src/MatrixTransformation2.cpp
@@ -28,48 +28,70 @@ Matrix3f::Matrix3f( float a[3][3] )
 }
 Matrix3f Matrix3f::Invert() 
 {
+    Matrix3f r;
+    float det = 0;
+    for(int i=0;i<3;i++)
+    {
+        det = det + (mat[0][i]*(mat[1][(i+1)%3]*
+        mat[2][(i+2)%3] - mat[1][(i+2)%3]*mat[2][(i+1)%3]));
+    }
+    if(det ==0)
+    {
+        std::cout<<"Inverse does not exist (Determinant=0).\n";
+    }
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)  {
+            r.mat[j][i] = ((mat[(i+1)%3][(j+1)%3] *
+                mat[(i+2)%3][(j+2)%3]) - (mat[(i+1)%3][(j+2)%3]*
+                mat[(i+2)%3][(j+1)%3]))/det;
+        }
+    }
     
-    float a[n][2*n];
-   
-    for (int i=0; i<n; ++i ) {
-        for (int j=0; j<2*n; ++j) {
-            if ( j < n )
-                a[i][j] = mat[i][j];
-            else {
-                if ( j == i + n)
-                    a[i][j] = 1;
-                else
-                    a[i][j] = 0;
-            }
-        }
-    }
-    float ratio;
-    for (int j = 0; j < n; ++j ) {
-        if ( fabs ( a[j][j] ) < 1e-7 ) {
-            std::cout<<"Matrix3fTransformation2::Can't inverse given Matrix3f\n";
-            assert(false);
-        }
-        for (int i=0; i < n; ++i) {
-            if ( i!=j ) {
-                ratio = a[i][j] / a[j][j];
-                for (int k =j ; k < 2*n ; ++k )
-                    a[i][k] -= ratio * a[j][k];
-            }
-        }
-    }
-    for (int i=0; i < n ; ++i) {
-        for (int j = n; j < 2*n; ++j ) {
-           a[i][j] = a[i][j] /a[i][i];       
-        }
-       
-    }
-    for ( int i=0; i<n; ++i ) {
-        for ( int j=0; j<m; ++j ) {
-            mat[i][j] = a[i][j+n];
-        }
-        
-    }
+    *this = r;
     return *this;
+    // float a[n][2*n];
+   
+    // for (int i=0; i<n; ++i ) {
+    //     for (int j=0; j<2*n; ++j) {
+    //         if ( j < n )
+    //             a[i][j] = mat[i][j];
+    //         else {
+    //             if ( j == i + n)
+    //                 a[i][j] = 1;
+    //             else
+    //                 a[i][j] = 0;
+    //         }
+    //     }
+    // }
+    // float ratio;
+
+    // for (int j = 0; j < n; ++j ) {
+    //     if ( fabs ( a[j][j] ) == 0.0 ) {
+    //         std::cout<<"Matrix3fTransformation2::Can't inverse given Matrix3f\n";
+    //         assert(false);
+    //     }
+    //     for (int i=0; i < n; ++i) {
+    //         if ( i!=j ) {
+    //             ratio = a[i][j] / a[j][j];
+    //             for (int k =j ; k < 2*n ; ++k )
+    //                 a[i][k] -= ratio * a[j][k];
+    //         }
+    //     }
+    // }
+    // for (int i=0; i < n ; ++i) {
+    //     for (int j = n; j < 2*n; ++j ) {
+    //        a[i][j] = a[i][j] /a[i][i];       
+    //     }
+       
+    // }
+    // for ( int i=0; i<n; ++i ) {
+    //     for ( int j=0; j<m; ++j ) {
+    //         mat[i][j] = a[i][j+n];
+    //     }
+        
+    // }
+    // return *this;
 }
 void Matrix3f::Print()
 {
