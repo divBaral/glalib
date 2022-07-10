62d193fab06bea0d3ce4ccbb1fb2371097616f9d
divBaral <076bct068.saroj@pcampus.edu.np>
2022-07-10 13:33:38 +0545

    updated

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -1,14 +1,18 @@
 #pragma once
-
+#include <cmath>
+#include <cassert>
 struct Vector
 {
     float x,y,z;
     const static int h = 0;
     Vector(float px=0, float py=0, float pz=0) ;
-    Vector operator+(const Vector& p);
-    Vector operator-(const Vector& p);
-    float operator%(const Vector& p); //dot product
-    Vector operator*(const Vector& p); //cros product
+    Vector operator+( Vector p);
+    Vector operator-( Vector p);
+    float operator%( Vector p); //dot product
+    float dot(Vector p);
+    Vector cross(Vector p);
+    Vector operator*( Vector p); //cros product
+    void normalize();
     
 
 };
--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,4 +1,5 @@
 #include "../include/Vector.hpp"
+#include <cmath>
 
 Vector::Vector(float px, float py, float pz) 
 {
@@ -6,22 +7,40 @@ Vector::Vector(float px, float py, float pz)
     y=py;
     z=pz;
 }
-Vector Vector::operator+(const Vector &p) 
+Vector Vector::operator+( Vector p) 
 {
     return Vector(x+p.x, y+p.y, z+p.z);
 }
-Vector Vector::operator-(const Vector &p) 
+Vector Vector::operator-( Vector p) 
 {
     return Vector(x-p.x, y-p.y, z-p.z);
 }
-float  Vector::operator%(const Vector &p) 
+float  Vector::operator%( Vector p) 
 {
     return p.x*x+p.y*y+p.z*z;
 }
-Vector  Vector::operator*(const Vector &p) 
+float Vector::dot( Vector p) {
+    return *this%p;
+}
+Vector  Vector::operator*( Vector p) 
 {
     float x = y*p.z - z*p.y;
     float y = z*p.x - x*p.z;
     float z = x*p.y - y*p.x;
     return Vector(x,y,z) ;
+}
+Vector  Vector::cross(Vector p)
+{
+    float x = y*p.z - z*p.y;
+    float y = z*p.x - x*p.z;
+    float z = x*p.y - y*p.x;
+    return Vector(x,y,z) ;
+}
+void Vector::normalize()
+{
+    float d = std::sqrt(x*x+y*y+z*z);
+    assert( d!= 0);
+    x = x/d;
+    y = y/d;
+    z = z/d;
 }
