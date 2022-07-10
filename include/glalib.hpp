d02c436577f5fbccb647246ff6c6746a9f649a2c
divBaral <076bct068.saroj@pcampus.edu.np>
2022-07-10 13:13:15 +0545

    updated

--- include/glalib.hpp
+++ include/glalib.hpp
@@ -3,3 +3,6 @@
 #include "MatrixTransformation.hpp"
 #include "MatrixTransformation.hpp"
 #include "AffineTransformation2.hpp"
+#include "AffineTransformation2.hpp"
+typedef AffineTransformation2 af2;
+typedef AffineTransformation af;
